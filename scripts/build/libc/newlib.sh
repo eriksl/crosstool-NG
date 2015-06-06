60ad4ce8323063615bfbd21b467cd5efec389595
Max Filippov <jcmvbkbc@gmail.com>
2015-06-06 21:49:49 +0300

    newlib.sh: copy xtensa-specific headers
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- scripts/build/libc/newlib.sh
+++ scripts/build/libc/newlib.sh
@@ -14,6 +14,11 @@ newlib_headers()
         CT_DoExecLog ALL cp -r "${CT_SRC_DIR}/newlib/newlib/libc/sys/xtensa/include/."   \
                                "${CT_HEADERS_DIR}"
     fi
+    if [ "${CT_ARCH_xtensa}" = "y" ]; then
+        CT_DoLog EXTRA "Installing Xtensa headers"
+        CT_DoExecLog ALL cp -r "${CT_SRC_DIR}/newlib-${CT_LIBC_VERSION}/newlib/libc/sys/xtensa/include/."   \
+                               "${CT_HEADERS_DIR}"
+    fi
     CT_EndStep
 }
 
