a7edbd90c55998b9042f329f92ac199d536c661a
Max Filippov <jcmvbkbc@gmail.com>
2016-02-19 05:03:43 +0300

    newlib.sh: remove duplicate xtensa-specific headers copying
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- scripts/build/libc/newlib.sh
+++ scripts/build/libc/newlib.sh
@@ -15,11 +15,6 @@ newlib_start_files()
         CT_DoExecLog ALL cp -r "${CT_SRC_DIR}/newlib/newlib/libc/sys/xtensa/include/."   \
                                "${CT_HEADERS_DIR}"
     fi
-    if [ "${CT_ARCH_xtensa}" = "y" ]; then
-        CT_DoLog EXTRA "Installing Xtensa headers"
-        CT_DoExecLog ALL cp -r "${CT_SRC_DIR}/newlib-${CT_LIBC_VERSION}/newlib/libc/sys/xtensa/include/."   \
-                               "${CT_HEADERS_DIR}"
-    fi
     CT_EndStep
 }
 
